dde90ea8e7f2ac758eba5047c9678828f65fb60a
Erich Purpur <epurpur@gmail.com>
2021-04-20 08:50:51 -0400

    adding intial logic

--- index.js
+++ index.js
@@ -1,13 +1,65 @@
-// TODO: Include packages needed for this application
+//Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
-// TODO: Create an array of questions for user input
-const questions = [];
 
-// TODO: Create a function to write README file
+//Create an array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: "Enter project name: "
+    },
+    {
+        type: 'input',
+        name: 'name',
+        message: "Write project description: "   
+    }
+    // 'Enter project name: ',
+    // 'Choose license type: ',
+    // 'Write description of project: ',
+    // 'Enter installation instructions: ',
+    // 'Enter project usage information ',
+    // 'Enter project contribution guidelines ',
+    // 'Enter project test instructions '
+];
+
+const licenseChoices = [
+    'Apache License 2.0',
+    'GNU General Public License v3.0',
+    'MIT License',
+    'BSD 2-Clause "Simplified" License',
+    //maybe add other choices?
+]
+
+//Create a function to write README file
 function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
-function init() {}
+//Function to prompt user for input
+const promptUser = () => {
+    return inquirer.prompt([
+        //NEED FOR EACH LOOP HERE FOR EACH PROJECT
+        {
+            type: 'input',
+            name: 'name',
+            message: "Enter project name: "
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: "Write project description: "   
+        }
+    ])
+}
+
+//Create a function to initialize app
+const init = () => {
+    console.log('here');
+    promptUser()
+        .then((answers) =>{
+            console.log(answers);
+        })
+}
 
 // Function call to initialize app
 init();
